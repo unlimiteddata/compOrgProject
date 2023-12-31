665648bb69a28866954cf88d25cdd42e05be0557
amira <aisenber@mail.yu.edu>
2023-12-31 12:43:34 -0500

    working encode + decode, plus output text file

--- finalProject.c
+++ finalProject.c
@@ -1,153 +1,166 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdint.h>
 
-int is_ascii(uint8_t byte){
-    return byte < 0x80;
+// helper function to convert hex character to int
+int hexCharToInt(char c) {
+    if (isdigit(c)) { // if decimal digit
+        return c - '0';
+    } else if (isxdigit(c)) { // if hex digit
+        return tolower(c) - 'a' + 10;
+    } else {
+        return -1; // Invalid hexadecimal character
+    }
 }
 
-// function to encode a UTF-8 string with Unicode code points
-// notation: "\uXXXX"
+// helper function to convert hex string to int
+int hexStringToInt(char *hex) {
+    int result = 0;
+    while (*hex != '\0') {
+        int digit = hexCharToInt(*(hex++));
+        if (digit < 0) {
+            return -1; // Invalid hexadecimal string
+        }
+        result = result * 16 + digit;
+    }
+    return result;
+}
+
+
+int is_ascii(const char *input) {
+    unsigned char c = (unsigned char)(*input);
+    return c <= 127;
+}
+
 int my_utf8_encode(char *input, char *output) {
-    int i = 0; // input string index
-    int j = 0; // output string index
+    if (input == NULL || output == NULL) { // empty string input
+        return -1; // Invalid 
+    }
 
-    // loop until the end of the string:
-    while (input[i] != '\0') {
-        if (input[i] == '\\' && input[i + 1] == 'u') {
-            // Extract Unicode code point:
-            unsigned int code_point;
-            if (sscanf(&input[i + 2], "%4X", &code_point) != 1) {
-                // Error: invalid \uXXXX notation
-                return -1;
-            }
+    char *encoded = output; // store result 
 
-            // Encode the Unicode code point in UTF-8
-            if (code_point <= 0x7F) { // one byte encoding
-                output[j++] = (char)code_point;
-            } else if (code_point <= 0x7FF) { // two byte encoding
-                // first byte: right-shift the code point by 6 - extracts upper 5
-                // bits of original code point. mask the result with 0x1F
-                // (00011111 in binary) to consider only the five lower bits.
-                // combine with leading prefix: 110 (0xC0)
-                output[j++] = (char)(0xC0 | ((code_point >> 6) & 0x1F));
-                output[j++] = (char)(0x80 | (code_point & 0x3F));
-            } else if (code_point <= 0xFFFF) { // three byte encoding
-                output[j++] = (char)(0xE0 | ((code_point >> 12) & 0x0F));
-                output[j++] = (char)(0x80 | ((code_point >> 6) & 0x3F));
-                output[j++] = (char)(0x80 | (code_point & 0x3F));
-            } else if (code_point <= 0x10FFFF) { // four byte encoding
-                output[j++] = (char)(0xF0 | ((code_point >> 18) & 0x07));
-                output[j++] = (char)(0x80 | ((code_point >> 12) & 0x3F));
-                output[j++] = (char)(0x80 | ((code_point >> 6) & 0x3F));
-                output[j++] = (char)(0x80 | (code_point & 0x3F));
+    while (*input != '\0') {
+        if (*input == 'U' && *(input + 1) == '+') {
+            input += 2; // Move past "U+"
+
+            // Convert hexadecimal string to integer
+            char hex[7] = {0}; // 6 hex digits + null terminator
+            int i;
+            for (i = 0; i < 6 && isxdigit(*input); ++i) {
+                hex[i] = *(input++); 
+            } 
+            int codePoint = hexStringToInt(hex);
+
+            // Encode the Unicode code point into UTF-8 using bit shifts 
+            if (codePoint <= 0x7F) { // one byte encoding
+                *(encoded++) = (char)codePoint;
+            } else if (codePoint <= 0x7FF) { // two byte encoding
+                // first byte: right-shift code point by 6 - gets upper 5
+                // bits of code point. mask result with 0x1F (0001111 in binary) 
+                // to consider only five lower bits; combine with leading 
+                // prefix 110 (0xC0) 
+                *(encoded++) = (char)(0xC0 | ((codePoint >> 6) & 0x1F));
+                *(encoded++) = (char)(0x80 | (codePoint & 0x3F));
+            } else if (codePoint <= 0xFFFF) { // three byte encoding
+                *(encoded++) = (char)(0xE0 | ((codePoint >> 12) & 0x0F));
+                *(encoded++) = (char)(0x80 | ((codePoint >> 6) & 0x3F));
+                *(encoded++) = (char)(0x80 | (codePoint & 0x3F));
+            } else if (codePoint <= 0x10FFFF) { // four byte encoding
+                *(encoded++) = (char)(0xF0 | ((codePoint >> 18) & 0x07));
+                *(encoded++) = (char)(0x80 | ((codePoint >> 12) & 0x3F));
+                *(encoded++) = (char)(0x80 | ((codePoint >> 6) & 0x3F));
+                *(encoded++) = (char)(0x80 | (codePoint & 0x3F));
             } else {
-                // Error: invalid Unicode code point
-                return -1;
+                return -1; // Invalid Unicode code point
             }
-
-            // Move to the next character in the input (skipping
-            i += 6; // Move to the character after \uXXXX
         } else {
-            // Copy non-'\\uXXXX' characters without modifying
-            output[j++] = input[i++];
+            *(encoded++) = *(input++);
         }
     }
 
-    // Null-terminate the output string
-    output[j] = '\0';
-
-    return 0;
+    *encoded = '\0'; // Null-terminate the encoded string
+    return 0; // Success
 }
 
 int my_utf8_decode(char *input, char *output) {
-    if (input == NULL || output == NULL) {
-        return -1; // Invalid input parameters
-    }
-
-    int outputIndex = 0;
-
     while (*input != '\0') {
-        uint8_t currentByte = (uint8_t)(*input);
-
-        // Determine the number of bytes in the UTF-8 character
-        int numBytes = 0;
-
-        // Decode the initial UTF-8 byte
-        if ((currentByte & 0x80) == 0) {
-            // Single-byte character
-            numBytes = 1;
-        } else if ((currentByte & 0xE0) == 0xC0) {
-            // Two-byte character
-            numBytes = 2;
-        } else if ((currentByte & 0xF0) == 0xE0) {
-            // Three-byte character
-            numBytes = 3;
-        } else if ((currentByte & 0xF8) == 0xF0) {
-            // Four-byte character
-            numBytes = 4;
+        if (is_ascii(input)) {
+            *output = *input;
+            ++input;
+            ++output;
         } else {
-            return -1; // Invalid UTF-8 sequence
-        }
-
-        // Decode the UTF-8 character
-        uint32_t codePoint = currentByte & (0xFF >> (numBytes + 1));
-        for (int i = 1; i < numBytes; i++) {
-            currentByte = (uint8_t)(*(++input));
-            if ((currentByte & 0xC0) != 0x80) {
-                return -1; // Invalid continuation byte
+            // Non-ASCII character, handle UTF-8 decoding
+            uint32_t codePoint = 0;
+            int  numBytes = 0;
+            // Determine the number of bytes in the UTF-8 sequence
+            if ((*input & 0b10000000) == 0) {
+                // Single-byte character
+                codePoint = *input;
+                 numBytes = 1;
+            } else if ((*input & 0b11100000) == 0b11000000) {
+                // Two-byte character
+                codePoint = *input & 0b00011111;
+                 numBytes = 2;
+            } else if ((*input & 0b11110000) == 0b11100000) {
+                // Three-byte character
+                codePoint = *input & 0b00001111;
+                 numBytes = 3;
+            } else if ((*input & 0b11111000) == 0b11110000) {
+                // Four-byte character
+                codePoint = *input & 0b00000111;
+                 numBytes = 4;
             }
-            codePoint = (codePoint << 6) | (currentByte & 0x3F);
-        }
 
-        // Convert the code point to ASCII representation if possible
-        if (is_ascii((uint8_t)codePoint)) {
-            output[outputIndex++] = (char)codePoint;
-        } else {
-            // Use UTF-8 character representation for non-ASCII characters
-            int numBytesWritten = snprintf(output + outputIndex, 7, "\\u%04X", codePoint);
-            if (numBytesWritten <= 0 || numBytesWritten > 6) {
-                return -1; // Error in formatting UTF-8 character representation
+            // Read the remaining bytes of the UTF-8 sequence
+            for (int i = 1; i <  numBytes; ++i) {
+                if ((input[i] & 0b11000000) != 0b10000000) {
+                    // Malformed UTF-8 sequence
+                    return -1;
+                }
+                codePoint = (codePoint << 6) | (input[i] & 0b00111111);
             }
-            outputIndex += numBytesWritten;
-        }
 
-        // Move to the next byte in the input string
-        input++;
+            int digits = 1;
+            uint32_t  temp = codePoint;
+            while (temp >>= 4){
+                ++digits;
+            }
+
+            int width = (digits < 4) ? 4 : digits;
+
+            sprintf(output, "U+%.*X", width, codePoint);
+
+            output += width + 2; // Move the output pointer to the end of the codePoint
+            input +=  numBytes; // Move the input pointer to the next character
+        }
     }
 
-    // Null-terminate the output string
-    output[outputIndex] = '\0';
-
-    return 0; // Success
+    *output = '\0'; // Null-terminate the output string
+    return 0;       // Success
 }
 
 
 int main() {
-    // ex input: "\\u05D0\\u05DE\\u05d9\\u05e8\\u05d4"
-   // char input1[] = "\\u05D0\\u05DE\\u05d9\\u05e8\\u05d4";
-    char output1[10];
-    char input1[45];
-    printf("Enter an ASCII string with UTF-8 characters: ");
-    scanf("%s", input1);
+    char input1[] = "Hello U+0048U+0065U+006CU+006CU+006FU+0021 U+1F601 U+5D0";
+    printf("Input: %s\n", input1);
+    char output1[100];
 
     if (my_utf8_encode(input1, output1) == 0) {
-        printf("Encoded UTF-8 string: %s\n", output1);
+        printf("Encoded string: %s\n", output1);
     } else {
-        printf("Error: Invalid \\uXXXX notation or Unicode code point.\n");
+        printf("Encoding failed.\n");
     }
 
-    char input2[45];
-    printf("Enter a UTF8 string: ");
-    scanf("%s", input2);
 
-    char output2[100];
+    char input2[] = "😁 Hello, אמירה";
+    printf("Input: %s\n", input2);
+    char output2[10000];
 
-    int result = my_utf8_decode(input2,output2);
-    if (result == 0){
-        printf("Decoded string: %s", output2);
-  }
-    else {
-        printf("Error decoding UTF-8 string.\n");
+    if (my_utf8_decode(input2, output2) == 0) {
+        printf("Decoded string: %s\n", output2);
+    } else {
+        printf("Decoding failed.\n");
     }
     return 0;
+
 }
